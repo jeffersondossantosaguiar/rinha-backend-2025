5fa56dffeedf67af8dcf309ae4d90cb3164393db
Jefferson Aguiar <jefferson.aguiar@facens.br>
2025-07-24 17:57:51 -0300

    chore: add http client

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,33 @@
 import { FastifyInstance } from "fastify"
+import { request } from "undici"
+import { dispatcher } from "./http-client.js"
+
+// TODO: Testar early return
+
+const PAYMENT_PROCESSOR_DEFAULT_URL =
+  process.env.PAYMENT_PROCESSOR_DEFAULT_URL || "http://localhost:8001"
 
 export default async function routes(app: FastifyInstance) {
-  app.post("/payments", async (request, reply) => {
-    return reply.code(201).send({ message: "Pagamento processado" })
+  app.post("/payments", async (req, reply) => {
+    const { correlationId, amount } = req.body as any
+
+    const payload = {
+      correlationId,
+      amount,
+      requestedAt: new Date().toISOString()
+    }
+
+    const { body, statusCode } = await request(
+      `${PAYMENT_PROCESSOR_DEFAULT_URL}/payments`,
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(payload),
+        dispatcher
+      }
+    )
+
+    return reply.code(201).send()
   })
 
   app.get("/payments-summary", async (request, reply) => {
