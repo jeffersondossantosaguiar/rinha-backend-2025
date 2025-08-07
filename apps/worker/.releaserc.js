b27d6275f17115c622741ae350fdaf8e1b6e7c47
Jefferson Aguiar <jefferson.aguiar@facens.br>
2025-08-07 14:36:51 -0300

    chore: add plugins to releaserc

--- apps/worker/.releaserc.js
+++ apps/worker/.releaserc.js
@@ -1,5 +1,10 @@
 export default {
   extends: "semantic-release-monorepo",
   tagFormat: "${packageName}-v${version}",
-  branches: ["main"]
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
 }
